c029fb28c3e1ca4a6d445e3859eaa54f0272cb3a
Name <39023769+C0NTRIBUTE@users.noreply.github.com>
2018-10-11 19:12:18 +0300

    added bi-directional LSTM

--- deepmark/bidir_lstm.py
+++ deepmark/bidir_lstm.py
@@ -3,45 +3,42 @@ Output after 4 epochs on CPU: ~0.8146
 Time per epoch on CPU (Core i7): ~150s.
 '''
 
-from __future__ import print_function
 import numpy as np
-
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.datasets import imdb
 
-
-max_features = 20000
-# cut texts after this number of words
-# (among top max_features most common words)
-maxlen = 100
-batch_size = 32
-
-print('Loading data...')
+#load dataset
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
 
-print('Pad sequences (samples x time)')
+# prepare data
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
+# define model
 model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(Embedding(20000, 128, input_length=100))
 model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
-# try using different optimizers and different optimizer configs
+# compile model
 model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
 
-print('Train...')
+# TEST BEGINS
+t0 = time.time()
+
 model.fit(x_train, y_train,
-          batch_size=batch_size,
+          batch_size=32,
           epochs=4,
-validation_data=[x_test, y_test])
+          validation_data=[x_test, y_test], verbose=0)
+t1=time.time()
+# TEST ENDS 
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print(t1-t0)
+print('test_acc:', test_acc)
