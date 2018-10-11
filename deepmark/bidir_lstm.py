5c92f77e48a8dd1c368e3f608604bd0274c7da9e
Name <39023769+C0NTRIBUTE@users.noreply.github.com>
2018-10-11 19:21:37 +0300

    Update bidir_lstm.py

--- deepmark/bidir_lstm.py
+++ deepmark/bidir_lstm.py
@@ -9,36 +9,38 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.datasets import imdb
 
-#load dataset
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+def bidir_lstm():
 
-# prepare data
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+    #load dataset
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 
-# define model
-model = Sequential()
-model.add(Embedding(20000, 128, input_length=100))
-model.add(Bidirectional(LSTM(64)))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+    # prepare data
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
 
-# compile model
-model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+    # define model
+    model = Sequential()
+    model.add(Embedding(20000, 128, input_length=100))
+    model.add(Bidirectional(LSTM(64)))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-# TEST BEGINS
-t0 = time.time()
+    # compile model
+    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=32,
-          epochs=4,
-          validation_data=[x_test, y_test], verbose=0)
-t1=time.time()
-# TEST ENDS 
+    # TEST BEGINS
+    t0 = time.time()
+    model.fit(x_train, y_train,
+              batch_size=32,
+              epochs=4,
+              validation_data=[x_test, y_test], verbose=0)
+    t1=time.time()
+    # TEST ENDS 
 
-test_loss, test_acc = model.evaluate(x_test, y_test)
+    # evaluate results
+    test_loss, test_acc = model.evaluate(x_test, y_test)
 
-print(t1-t0)
-print('test_acc:', test_acc)
+    print(t1-t0)
+    print('test_acc:', test_acc)
