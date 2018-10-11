e1a94dd44ee7dde78fc247ff86817f96b3e96c4d
Name <39023769+C0NTRIBUTE@users.noreply.github.com>
2018-10-11 19:19:22 +0300

    Update minst.py

--- deepmark/minst.py
+++ deepmark/minst.py
@@ -15,34 +15,36 @@ from keras.layers import Dense
 from keras.utils import to_categorical
 from keras.datasets import mnist
 
-# load dataset
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+def minst():
 
-# prepare data
-train_images = train_images.reshape((60000, 28 * 28))
-train_images = train_images.astype('float32') / 255
-test_images = test_images.reshape((10000, 28 * 28))
-test_images = test_images.astype('float32') / 255
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+    # load dataset
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# define model
-model = models.Sequential()
-model.add(Dense(512, activation='relu', input_shape=(28 * 28,)))
-model.add(Dense(10, activation='softmax'))
+    # prepare data
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype('float32') / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype('float32') / 255
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
 
-# compile model
-model.compile(optimizer='rmsprop',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
+    # define model
+    model = models.Sequential()
+    model.add(Dense(512, activation='relu', input_shape=(28 * 28,)))
+    model.add(Dense(10, activation='softmax'))
 
-# TEST BEGINS
-t0 = time.time()
-model.fit(train_images, train_labels, epochs=10, batch_size=256, verbose=0)
-t1=time.time()
-# TEST ENDS 
+    # compile model
+    model.compile(optimizer='rmsprop',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+    # TEST BEGINS
+    t0 = time.time()
+    model.fit(train_images, train_labels, epochs=10, batch_size=256, verbose=0)
+    t1=time.time()
+    # TEST ENDS 
 
-print(t1-t0)
-print('test_acc:', test_acc)
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print(t1-t0)
+    print('test_acc:', test_acc)
