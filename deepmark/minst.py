b0bc2fd7d7e6cb1f096f15e25c3d211c259f70d8
Name <39023769+C0NTRIBUTE@users.noreply.github.com>
2018-10-11 18:56:21 +0300

    Update minst.py

--- deepmark/minst.py
+++ deepmark/minst.py
@@ -1,30 +1,43 @@
+'''
+BENCHMARKS: 
+
+0.3 seconds on i7-7700k & 1070Ti
+0.3 seconds on i7-7700k (force CPU)
+1.3 seconds on i3-6100 & 1050Ti
+
+'''
+
 import time
 import tensorflow as tf
 import keras
-from keras import models
-from keras import layers
-
-from keras.backend.tensorflow_backend import set_session
-
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
 from keras.datasets import mnist
 
+# load dataset
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-model = models.Sequential()
-network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
-network.add(layers.Dense(10, activation='softmax'))
-network.compile(optimizer='rmsprop',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
+# prepare data
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
-from keras.utils import to_categorical
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
+
+# define model
+model = models.Sequential()
+model.add(Dense(512, activation='relu', input_shape=(28 * 28,)))
+model.add(Dense(10, activation='softmax'))
+
+# compile model
+model.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
 t0 = time.time()
-network.fit(train_images, train_labels, epochs=10, batch_size=256, verbose=0)
+model.fit(train_images, train_labels, epochs=10, batch_size=256, verbose=0)
 test_loss, test_acc = network.evaluate(test_images, test_labels)
 t1=time.time()
 print(t1-t0)
